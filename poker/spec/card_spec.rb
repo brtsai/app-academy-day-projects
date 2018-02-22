15cc943de3ad1907283e2617eec0e34b3a000f4e
Bryan Tsai <bryan@bryantsaiwh.com>
2018-02-21 17:26:10 -0800

    Start working on 5-hand poker

---
+++ poker/spec/card_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require 'card'
+
+describe Card do
+  subject(:card) { Card.new }
+
+  describe "#initialize" do
+    it "Should set a suit and value" do
+      
+    end
+  end
+
+end
