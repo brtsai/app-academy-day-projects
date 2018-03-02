c00479cd0e0dfec6c9cdda5f4a6b3d47ac0a3ab7
Bryan Tsai <bryan@bryantsaiwh.com>
2018-03-02 14:30:26 -0800

    implement part of SQLObject SQLObject::my_attr_accessor ::table_name ::table_name= ::columns ::finalize! \#attributes \#initialize

--- w3d5/active_record_lite_i/lib/00_attr_accessor_object.rb
+++ w3d5/active_record_lite_i/lib/00_attr_accessor_object.rb
@@ -1,5 +1,16 @@
+require 'byebug'
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     # ...
+    names.each do |name|
+      instance_name = "@#{name}".to_sym
+      define_method(name) do
+        instance_variable_get(instance_name)
+      end
+      name_eq = "#{name}=".to_sym
+      define_method(name_eq) do |value|
+        instance_variable_set(instance_name, value)
+      end
+    end
   end
 end
--- w3d5/active_record_lite_i/lib/01_sql_object.rb
+++ w3d5/active_record_lite_i/lib/01_sql_object.rb
@@ -6,21 +6,52 @@ require 'active_support/inflector'
 class SQLObject
   def self.columns
     # ...
+    if @columns.nil?
+      data = DBConnection.execute2(<<-SQL, )
+        SELECT
+          *
+        FROM
+          #{self.table_name}
+        LIMIT
+          1
+      SQL
+      col_names = data.first
+      @columns = col_names.map { |str| str.to_sym }
+    end
+
+    @columns
   end
 
   def self.finalize!
+    self.columns.each do |col_name|
+      define_method(col_name) do
+        attributes[col_name]
+      end
+
+      col_name_eq = "#{col_name}=".to_sym
+      define_method(col_name_eq) do |val|
+        attributes[col_name] = val
+      end
+
+    end
   end
 
   def self.table_name=(table_name)
     # ...
+    @table_name = table_name
   end
 
   def self.table_name
     # ...
+    if @table_name.nil?
+      @table_name = self.name.tableize
+    end
+    @table_name
   end
 
   def self.all
     # ...
+  
   end
 
   def self.parse_all(results)
@@ -33,10 +64,24 @@ class SQLObject
 
   def initialize(params = {})
     # ...
+    params.each do |attr_name, value|
+      attr_name_sym = attr_name.to_sym
+      unless self.class.columns.include?(attr_name_sym)
+        raise "unknown attribute '#{attr_name}'"
+      end
+      
+      attr_name_eq = "#{attr_name_sym}=".to_sym
+      self.send(attr_name_eq, value)
+  
+    end
   end
 
   def attributes
     # ...
+    if @attributes.nil?
+      @attributes = {}
+    end
+    @attributes
   end
 
   def attribute_values
