df489839f2f50faffd43be4da1dc165ef344ffd1
Bryan Tsai <bryan@bryantsaiwh.com>
2018-03-02 17:05:30 -0800

    added workaround for known rails inflector tableize bug https://github.com/rails/rails/issues/17671

--- w3d5/active_record_lite_i/lib/03_associatable.rb
+++ w3d5/active_record_lite_i/lib/03_associatable.rb
@@ -1,5 +1,6 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
+require 'byebug'
 
 # Phase IIIa
 class AssocOptions
@@ -11,22 +12,42 @@ class AssocOptions
 
   def model_class
     # ...
+    self.class_name.constantize
   end
 
   def table_name
     # ...
+    return "humans" if self.class_name.downcase == "human"
+    self.class_name.tableize
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     # ...
+    
+    self.primary_key= 'id'.to_sym
+    self.foreign_key= "#{name}_id".to_sym
+    self.class_name = name.camelcase
+
+    options.each do |attr, value|
+      attr_eq = "#{attr}=".to_sym
+      self.send(attr_eq, value)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     # ...
+    self.primary_key= 'id'.to_sym
+    self.foreign_key= "#{self_class_name.underscore}_id".to_sym
+    self.class_name = name.singularize.camelcase
+    
+    options.each do |attr, value|
+      attr_eq = "#{attr}=".to_sym
+      self.send(attr_eq, value)
+    end
   end
 end
 
