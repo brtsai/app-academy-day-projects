65dc1024e8d2658f63267c141eb05c30470d5baa
Bryan Tsai <bryan@bryantsaiwh.com>
2018-03-19 18:14:33 -0700

    moved W5D5 into W5D5

---
+++ W5D5/css-friends-06/images/index_files/bundle.js
@@ -0,0 +1,172 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+const MovingObject = function (options) {
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+};
+
+
+
+MovingObject.prototype.draw = function (ctx) {
+  console.log(this);
+
+ ctx.fillStyle = this.color;
+ ctx.beginPath();
+
+ ctx.arc(
+   this.pos[0],
+   this.pos[1],
+   this.radius,
+   0,
+   2 * Math.PI,
+   false
+ );
+
+ ctx.fill();
+};
+
+module.exports = MovingObject;
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let MovingObject = __webpack_require__(0);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let Asteroid = __webpack_require__(6);
+let Bullet = __webpack_require__(1);
+let Ship = __webpack_require__(3);
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let MovingObject = __webpack_require__(0);
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const Bullet = __webpack_require__(1);
+const GameView = __webpack_require__(5);
+const Game = __webpack_require__(2);
+const MovingObject = __webpack_require__(0);
+const Ship = __webpack_require__(3);
+const Utils = __webpack_require__(7);
+
+document.addEventListener("DOMContentLoaded", function(){
+const canvasEl = document.getElementById("game-canvas");
+  canvasEl.width = 500;
+  canvasEl.height = 500;
+
+const ctx = canvasEl.getContext("2d");
+ctx.fillStyle = "purple";
+ctx.fillRect(0, 0, 500, 500);
+
+window.MovingObject = new MovingObject({pos: [30, 30], vel: [10, 10],
+                                       radius: 5, color: "#00FF00"});
+
+
+window.MovingObject.draw(ctx);
+ });
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let Game = __webpack_require__(2);
+
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+let MovingObject = __webpack_require__(0);
+
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+
+
+/***/ })
+/******/ ]);
